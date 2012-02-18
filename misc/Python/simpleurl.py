71d982e64529e1f652d38c672141fdce5df729be
chrisjohnriley@gmail.com <chrisjohnriley@gmail.com@9336ed2d-2af6-076c-6d2c-d638a2f11ec9>
2012-02-18 16:59:38 +0000

    added simpleurl code

---
+++ misc/Python/simpleurl.py
@@ -0,0 +1,353 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#
+
+"""
+    Example script to retrieve contents of a URL using Windows Functions
+
+    This script uses SSPI to reuse the current users credentials to make
+    the request (bypassing issues with restrictive Proxy servers (NTLM auth)
+"""
+
+from sys import exit, platform, argv
+from urlparse import urlparse
+from ctypes import *
+from ctypes.wintypes import DWORD
+from getopt import getopt
+from os import path
+from string import split
+
+__author__ = 'Chris John Riley'
+__license__ = 'GPL'
+__version__ = '0.1'
+__maintainer__ = 'Chris John Riley'
+__email__ = 'contact@c22.cc'
+__status__ = 'Prototype'
+
+kernel32 = windll.kernel32
+wininet = windll.wininet
+
+logo =\
+'''
+                                                   .   ..--. .
+                                                   |   ||   )|
+                                            .--.   |   ||--' |
+                                            `--.   :   ;|  \ |
+                                            `--'____`-' '   `'---'
+
+                                            ------------------------
+                                            | Simple url retriever |
+                                            ------------------------
+                                                    Chris John Riley
+                                                        blog.c22.cc
+'''
+
+usage = \
+'''
+\tusage:\n
+\t ./simpleurl.py -u <url> -o <outfile> -d
+
+\t\t -u <url> / --url=<url> - Specific URL to access
+\t\t -o <file> / --outfile=<file> -[opt:] Output file
+\t\t -d / --display - [opt:] Display retrieved URL
+
+\texample:\n
+\t ./simpleicmp.py -u http://c22.cc -d
+'''
+
+def main():
+
+    url, outfile, display = setup()
+    checks()
+    contents = urlrequest(url)
+    if display:
+        displayurl(contents, url)
+    if outfile:
+        saveurl(contents, outfile, url)
+
+def setup():
+
+    # setup variables
+
+    print logo
+
+    if len(argv) > 1:
+        SHORTOPTS = "hu:o:d"
+        LONGOPTS = ("help", "url=", "outfile=", "display")
+        try:
+            (opts, args) = getopt(argv[1:], SHORTOPTS, LONGOPTS)
+        except:
+            print usage
+            exit(0)
+
+        # set defaults
+        outfile =''
+        display = False
+
+        for (opt, arg) in opts:
+            if opt in ('-h', '--help'):
+                print usage
+                exit(0)
+            elif opt in ('-u', '--url'):
+                if arg.startswith('http'):
+                    url = arg
+                else:
+                    url = 'http://' + arg
+            elif opt in ('-o', '--outfile'):
+                outfile = arg
+            elif opt in ('-d', '--display'):
+                display = True
+
+        print ' [ ] URL: %s' % url
+
+        if outfile:
+            print ' [ ] Output will be save as %s' % outfile
+        if display:
+            print ' [ ] Output will be displayed'
+        if not outfile and not display:
+            print ' [!] No output or file selected... running silent!'
+
+        return url, outfile, display
+
+    else:
+        print usage
+        exit(0)
+
+
+def checks():
+
+    # check platform and connection
+
+    check_platform()
+    check_network()
+
+
+def check_platform():
+
+    # check target is 'winXX'
+
+    if not platform.startswith('win'):
+        print ' [!] Not a Windows system!, exiting'
+        exit(1)
+
+
+def check_network():
+
+    # check network connection
+
+    flags = DWORD()
+    print '\n [>] Checking connection'
+    connected = wininet.InternetGetConnectedState(
+                byref(flags),
+                None,
+                )
+
+    if not connected:
+        print ' [!] No internet connection, cannot retrieve data'
+        exit(1)
+    else:
+        print ' [<] Connection check confirmed'
+
+
+def urlrequest(url):
+
+    # attempt to use Internet Explorer (urlmon) to retrieve shellcode from a remote URL
+
+    INTERNET_OPEN_TYPE_PRECONFIG = 0
+    INTERNET_SERVICE_HTTP = 3
+    INTERNET_FLAG_RELOAD = 0x80000000
+    INTERNET_FLAG_CACHE_IF_NET_FAIL = 0x00010000
+    INTERNET_FLAG_NO_CACHE_WRITE = 0x04000000
+    INTERNET_FLAG_PRAGMA_NOCACHE = 0x00000100
+    INTERNET_FLAG_SECURE = 0x00800000
+    HTTP_QUERY_FLAG_NUMBER = 0x20000000
+    HTTP_QUERY_STATUS_CODE = 19
+    SECURITY_FLAG_IGNORE_REVOCATION = 0x00000080
+    SECURITY_FLAG_IGNORE_UNKNOWN_CA = 0x00000100
+    SECURITY_FLAG_IGNORE_WRONG_USAGE = 0x00000200
+    INTERNET_FLAG_IGNORE_REDIRECT_TO_HTTP = 0x00008000
+    INTERNET_FLAG_IGNORE_REDIRECT_TO_HTTPS = 0x00004000
+    INTERNET_FLAG_IGNORE_CERT_DATE_INVALID = 0x00002000
+    INTERNET_FLAG_IGNORE_CERT_CN_INVALID = 0x00001000
+    INTERNET_OPTION_SECURITY_FLAGS = 31
+
+    dwStatus = DWORD()
+    dwBufLen = DWORD(4)
+    dwFlags = DWORD()
+    buff = c_buffer(0x2000)
+    bytesRead = DWORD()
+    useragent = 'Mozilla/5.0'
+    method = 'GET'
+    data = ''
+
+
+    # parse url into peices for later use
+
+    p_url = urlparse(url)
+    path = p_url.path
+    netloc = p_url.netloc.split(':')
+    conn_user = p_url.username
+    conn_pass = p_url.password
+
+    if p_url.port:
+        conn_port = p_url.port
+    elif p_url.scheme == 'http':
+        conn_port = 80
+    else:
+        conn_port = 443
+
+    print '\n [>] Checking URL'
+
+    try:
+        hInternet = wininet.InternetOpenA(
+                    useragent,
+                    INTERNET_OPEN_TYPE_PRECONFIG,
+                    False,
+                    False,
+                    0,
+                    )
+
+        if not hInternet:
+            print ' [!] Unable to build connection to %s' % p_url.geturl()
+            raise Exception(('InternetOpenA Failed (Windows error %d)') % GetLastError())
+
+        hConnect = wininet.InternetConnectA(
+                    hInternet,
+                    netloc[0],
+                    conn_port,
+                    conn_user,
+                    conn_pass,
+                    INTERNET_SERVICE_HTTP,
+                    0,
+                    0,
+                    )
+
+        if not hConnect:
+            print ' [!] Unable to make connection to %s' % p_url.geturl()
+            raise Exception(('InternetConnectA Failed (Windows error %d)') % GetLastError())
+
+        dwFlags = \
+            INTERNET_FLAG_RELOAD | INTERNET_FLAG_NO_CACHE_WRITE | \
+            INTERNET_FLAG_CACHE_IF_NET_FAIL | INTERNET_FLAG_PRAGMA_NOCACHE
+
+        if p_url.scheme == 'https':
+            dwFlags |= INTERNET_FLAG_SECURE
+
+        hRequest = wininet.HttpOpenRequestA(
+                    hConnect,
+                    method,
+                    path,
+                    False,
+                    False,
+                    False,
+                    dwFlags,
+                    0,
+                    )
+
+        # ignore CA errors and redirects
+
+        dwFlags = \
+            'SECURITY_FLAG_IGNORE_REVOCATION | SECURITY_FLAG_IGNORE?_UNKNOWN_CA | \
+            SECURITY_FLAG_IGNORE_WRONG_USAGE | INTERNET_FLAG_IGNORE_CERT_CN_INVALID | \
+            INTERNET_FLAG_IGNORE_CERT_DATE_INVALID | INTERNET_FLAG_IGNORE_REDIRECT_TO_HTTPS | \
+            INTERNET_FLAG_IGNORE_REDIRECT_TO_HTTP'
+
+        sOptions = wininet.InternetSetOptionA(hRequest,
+                    INTERNET_OPTION_SECURITY_FLAGS,
+                    dwFlags,
+                    sizeof(DWORD)
+                    )
+
+        if not sOptions:
+            print ' [!] Unable to set internet options'
+            raise Exception(('InternetSetOptionA Failed (Windows error %d)') % GetLastError())
+
+        if not hRequest:
+            print ' [!] Unable to open request to %s' % p_url.geturl()
+            raise Exception(('HttpOpenRequestA Failed (Windows error %d)') % GetLastError())
+
+        hSendRequest = wininet.HttpSendRequestA(
+                        hRequest,
+                        False,
+                        0,
+                        False,
+                        0,
+                        )
+
+        if not hSendRequest:
+            print ' [!] Unable to send request to %s' % p_url.geturl()
+            raise Exception(('HttpSendRequestA Failed (Windows error %d)') % GetLastError())
+
+        hQuery = wininet.HttpQueryInfoA(
+                    hRequest,
+                    HTTP_QUERY_STATUS_CODE | HTTP_QUERY_FLAG_NUMBER,
+                    byref(dwStatus),
+                    byref(dwBufLen),
+                    False,
+                    )
+
+        if not hQuery:
+            print ' [!] Unable to complete query to %s' % p_url.geturl()
+            raise Exception(('HttpQueryInfoA Failed (Windows error %d)') % GetLastError())
+
+        if dwStatus.value != 200:
+            print ' [!] Incorrect server response from %s' % p_url.geturl()
+            raise Exception(('Non 200 OK response (Windows error %d)') % GetLastError())
+        else:
+            print ' [<] Server response %s received' % dwStatus.value
+
+        # conntection ok, read data (limited to buffer size)
+
+        hRead = wininet.InternetReadFile(
+                hRequest,
+                buff,
+                len(buff),
+                byref(bytesRead),
+                )
+
+        if not hRead:
+            print ' [!] Unable to read response from %s' % p_url.geturl()
+            raise Exception(('InternetReadFile Failed (Windows error %d)') % GetLastError())
+        else:
+            data = data + buff.raw[:bytesRead.value]
+            print ' [<] Reading in response from %s' % p_url.geturl()
+
+        return data
+
+        # teardown connections
+
+        wininet.InternetCloseHandle(hRequest)
+        wininet.InternetCloseHandle(hConnect)
+        wininet.InternetCloseHandle(hInternet)
+
+    except Exception, error:
+        print ' [!] Unable to retrieve URL (%s)' % p_url.geturl()
+        print ' [!] Error ::: %s' % error
+        exit(1)
+
+
+def displayurl(contents, url):
+
+    print '\n [>] Beginning output of %d bytes of data from %s\n' % (len(contents), url)
+    lines = split(contents, '\n')
+    for each in lines:
+        print '\t' + each
+    print '\n [<] Output complete'
+
+
+def saveurl(contents, outfile, url):
+
+    if path.exists(outfile):
+        print ' [!] Output file %s already exists!' % outfile
+        exit(0)
+
+    print '\n [>] Saving %d bytes of data from %s to %s' % (len(contents), url, outfile)
+    handle = open(outfile, 'w')
+    handle.write(contents)
+    handle.close()
+    print ' [<] Save complete'
+
+
+if __name__ == '__main__':
+    main()
